24419a2bf88d4f5cd48a483ec4f793031f3c6fab
Kevin <kevin@MacBook-Air-de-Celeste.local>
2026-07-14 14:28:27 +0200

    feat: swap two gallery photos to real photos and remove unused placeholders

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -7,8 +7,8 @@ type GalleryProps = {
 };
 
 const IMAGES = [
-  "/images/gallery-1.jpg",
-  "/images/gallery-2.jpg",
+  "/images/image-interieur1.jpeg",
+  "/images/image-coupe1.jpeg",
   "/images/gallery-3.jpg",
   "/images/gallery-4.jpg",
   "/images/gallery-5.jpg",
