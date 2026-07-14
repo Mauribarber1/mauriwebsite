19ac4e6e060ac99ff3f2593f2e72571957d6e232
Kevin <kevin@MacBook-Air-de-Celeste.local>
2026-07-14 13:45:32 +0200

    feat: enlarge navbar logo from h-10 to h-14

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -7,7 +7,7 @@ type LogoProps = {
 export default function Logo({ className = "" }: LogoProps) {
   return (
     <span className={`inline-flex items-center ${className}`}>
-      <Image src="/logo-noir.svg" alt="Mauri Studio" width={384} height={225} className="h-10 w-auto" priority />
+      <Image src="/logo-noir.svg" alt="Mauri Studio" width={384} height={225} className="h-14 w-auto" priority />
     </span>
   );
 }
